a93ef1f15a04baf3211d3f0938e38f2f700ec927
Jeremy <jeremy@zipcodes-MacBook-Pro-5.local>
2021-06-23 14:34:55 -0400

    Todays code with Leon about conditional statements

---
+++ dev/MyProject2/src/main/java/PracticeCode2.java
@@ -0,0 +1,30 @@
+public class PracticeCode2 {
+    public static void main(String[] args) {
+        myFirstIfStatement();
+    }
+        public static void raymondQuestionTest () {
+            //declaration
+            int x;
+//assignment
+            x = 5;
+// declaration & assignment (a type of initialization)
+            int y = 5;
+//declaration & then assignment (a type of initialization)
+            int z;
+            z = 5;
+        }
+
+        public static void ifThenIfNotStatement() {
+        {
+            boolean is5GreaterThan1 = (5 > 1);
+            String name = "Leon";
+            if(!is5GreaterThan1) {
+                System.out.println(name);
+            } else {
+                System.out.println("Not greater than 1");
+            }
+              // System.out.println("hello world");
+               // System.out.println("hey world");
+            }
+        }
+    }
